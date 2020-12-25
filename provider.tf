772b8ee9320e6a870d4ebf65102352e616c99d67
Cloud User <ec2-user@ip-172-31-43-248.ap-south-1.compute.internal>
2020-12-25 09:35:51 +0000

    newwww

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
 provider "aws" {
-#access_key = var.AWS_ACCESS_KEY
-#secret_key = var.AWS_SECRET_KEY
-region = "ap-south-1"
-<<<<<<< HEAD
-shared_credentials_file = "/home/ec2-user/.aws/credentials"
-=======
->>>>>>> 769f4a31a7bd45560eb79c0b0d9fd22aa2f01683
-profile = "default"
+  region                  = "ap-south-1"
+  shared_credentials_file = "/home/ec2-user/.aws/credentials"
+  profile                 = "default"
 }
